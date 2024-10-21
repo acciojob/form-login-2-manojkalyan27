9ea32ac8025609e67be1a32e5c9310c930cd3913
manojkalyan27 <147662541+manojkalyan27@users.noreply.github.com>
2024-10-21 15:12:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const submit = document.getElementById("Submit");
+
+submit.addEventListener("click",printMeassage);
+
+function printMeassage(event){
+  event.preventDefault();
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const phoneNumber = document.getElementById("number").value;
+  const emailID = document.getElementById("emailID").value;
+
+
+  const message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailID}`;
+                 
+  alert(message);
+
+}
